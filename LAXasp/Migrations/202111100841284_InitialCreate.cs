c3a04a26743cd61686f483b9d4ee583f790c6b26
DanielHoyerHare <danielhoyerhare@gmail.com>
2021-11-12 14:05:29 +0100

    gem

---
+++ LAXasp/Migrations/202111100841284_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿namespace LAXasp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Films",
+                c => new
+                    {
+                        FilmID = c.Int(nullable: false, identity: true),
+                        Titel = c.String(),
+                        Instruktør = c.String(),
+                        Årstal = c.Int(nullable: false),
+                        Længde = c.String(),
+                        Rating = c.Int(nullable: false),
+                        Genre = c.String(),
+                    })
+                .PrimaryKey(t => t.FilmID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Films");
+        }
+    }
+}
---
+++ LAXasp/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace LAXasp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<LAXasp.Models.FilmDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "LAXasp.Models.FilmDBContext";
+        }
+
+        protected override void Seed(LAXasp.Models.FilmDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
